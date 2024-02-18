188cb4b90ccc6bc0037697209eb9829bce547574
kiparvez <ndp943296@gmail.com>
2024-02-19 02:41:42 +0600

    change done

--- library_management/settings.py
+++ library_management/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://libary-management-sixy.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
